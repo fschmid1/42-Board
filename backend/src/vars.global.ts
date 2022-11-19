dd24b1a1b91fc61dea01d8cd4d4536cbd4dea7fd
Felix <felix.schmid@siriusonline.de>
2022-11-19 12:15:13 +0100

    d

--- backend/src/vars.global.ts
+++ backend/src/vars.global.ts
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || '8080';
 export const DB_URL = process.env.DB_URL;
-export const AUTH_UID = process.env['42_UID'];
-export const AUTH_SECRET = process.env['42_SECRET'];
+export const AUTH_UID = process.env['UID'];
+export const AUTH_SECRET = process.env['SECRET'];
