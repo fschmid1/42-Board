ad0a15f8185f41700ae90bca39d5bbe877a3540d
Felix <felix.schmid@festech.de>
2022-11-29 16:37:07 +0100

    Reactions working

--- frontend/src/lib/Reactions.svelte
+++ frontend/src/lib/Reactions.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  import { Button } from 'flowbite-svelte';
+  import { Popover } from 'flowbite-svelte';
   import { apiBaseEndpoint } from '../variables';
   import EmojiSelector from 'svelte-emoji-selector';
+  import Avatar from './Avatar.svelte';
+  import type { Reaction } from '../interfaces/reaction.interface';
 
   export let id;
-  export let reactions;
+  export let reactions: Reaction[] = [];
 
   const submit = async emote => {
     const res = await fetch(apiBaseEndpoint + 'reaction/comments', {
@@ -24,9 +26,20 @@
 </script>
 
 {#if reactions}
-  <div class="flex absolute -top-2 right-0 justify-en ">
+  <div class="flex absolute -top-2 right-0 justify-end">
     {#each reactions as r}
+      <Popover class="w-44" title={r.emote} trigger="hover" triggeredBy="#reaction-{r.id}">
+        <div class="flex flex-col">
+          {#each r.users as user}
+            <div class="flex w-fit justify-star">
+              <Avatar size="4" src={user.user.photoUrl} />
+              <span class="ml-2 ">{user.user.username}</span>
+            </div>
+          {/each}
+        </div>
+      </Popover>
       <span
+        id="reaction-{r.id}"
         on:click={() => {
           submit({ detail: r.emote });
         }}
@@ -34,9 +47,7 @@
         >{r.emote} <span class="text-sm text-center">{r.count}</span></span
       >
     {/each}
-    <span class="reaction text-xl px-2 text-center rounded-lg shadow bg-gray-900"
-      ><EmojiSelector class="z-50" on:emoji={submit} /></span
-    >
+    <span class="text-xl px-2 text-center rounded-lg shadow bg-gray-900"><EmojiSelector class="z-50" on:emoji={submit} /></span>
   </div>
 {/if}
 
